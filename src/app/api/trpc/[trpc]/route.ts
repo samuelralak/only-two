189491ccd3ea70ba0844d8502a317bf09a600892
Samuel Ralak <thesamuelralak@gmail.com>
2023-10-25 00:10:09 +0300

    chore: fix build errors

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -4,6 +4,12 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
 import dbConnect from "@/server/mongoose";
 import {seedDatabase} from "@/server/mongoose/seeder";
 
+const getBaseUrl = () => {
+    if (typeof window !== 'undefined') return ''; // browser should use relative url
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+    if (process.env.INTERNAL_ADDRESS) return `http://${process.env.INTERNAL_ADDRESS}:${process.env.PORT ?? 3000}`;
+    return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
 
 const requestHandler = (req: Request) =>
     fetchRequestHandler({
--- src/components/TrpcProvider.tsx
+++ src/components/TrpcProvider.tsx
@@ -4,11 +4,18 @@ import client from "@/lib/client";
 import {httpBatchLink} from "@trpc/client";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
+const getBaseUrl = () => {
+    if (typeof window !== 'undefined') return ''; // browser should use relative url
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+    if (process.env.INTERNAL_ADDRESS) return `http://${process.env.INTERNAL_ADDRESS}:${process.env.PORT ?? 3000}`;
+    return `http://127.0.0.1:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
 const queryClient = new QueryClient()
 const trpcClient =  client.createClient({
     links: [
         httpBatchLink({
-            url: `/api/trpc`,
+            url: `${getBaseUrl()}/api/trpc`,
             async headers() {
                 return {
                     // authorization: getAuthCookie(),
