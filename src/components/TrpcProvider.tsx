7f35dde248c1b31736b15168e270061d30315578
Samuel Ralak <thesamuelralak@gmail.com>
2023-10-25 01:10:26 +0300

    chore: log base url in build

--- src/components/TrpcProvider.tsx
+++ src/components/TrpcProvider.tsx
@@ -7,9 +7,11 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 const getBaseUrl = () => {
     if (typeof window !== 'undefined') return ''; // browser should use relative url
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-    if (process.env.INTERNAL_ADDRESS) return `http://${process.env.INTERNAL_ADDRESS}:${process.env.PORT ?? 3000}`;
+    if (process.env.INTERNAL_ADDRESS) return `https://${process.env.INTERNAL_ADDRESS}:${process.env.PORT ?? 3000}`;
     return `http://127.0.0.1:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
+
+console.log(getBaseUrl())
 
 const queryClient = new QueryClient()
 const trpcClient =  client.createClient({
